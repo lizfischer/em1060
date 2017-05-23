0b480fa113baf082b9434d5a9e21ecfc1865077c
lizfischer <lizzy.m.fischer@gmail.com>
2017-05-23 16:14:14 -0700

    working on bib

--- python/getBib.py
+++ python/getBib.py
@@ -1,27 +1,50 @@
 from bs4 import BeautifulSoup
 from collections import OrderedDict
-import urllib, json, pprint
+import urllib, json, pprint, re
 pp = pprint.PrettyPrinter(indent=4)
 
 r = urllib.urlopen('https://www.le.ac.uk/english/em1060to1220/bibliography.htm')
 soup = BeautifulSoup(r,'lxml')
 
-list = soup.find("div", class_="feature").contents[3:19]
+list = soup.find("div", class_="feature").contents[3:]
+
 
 prevName = None
-for item in list:
-    try:
-        id = item.get('id')
-        content = "".join(str(x) for x in item.contents)
-        if (content[:3] == '---'):
-            name = prevName
-        else:
-            name = content.split(',')[0] + ","+ content.split(',')[1]
-            prevName = name
 
-        print(content) #TODO replace with file write!
+i=1
+with open('bib2.json', 'w') as f, open('bibErrors', 'w') as e:
+    f.write("[")
+    for item in list:
+        try:
+            id = item.get('id')
+            content = "".join(str(x.encode('utf-8')) for x in item.contents).replace('"', "'")
 
-    except:
-        #print(item)
-        pass
+            if (content[:3] == '---'):
+                name = prevName
+                citation = "".join(str(x) for x in content.split(',')[1:])
+            else:
+                name = content.split(',')[0] + ","+ content.split(',')[1].strip()
+                prevName = name
+                citation = ",".join(str(x) for x in content.split(',')[2:]).strip()
 
+            year = ""
+            year = re.compile('(\d\d\d\d)\)')
+            year = year.search(citation).group(1)
+            citation = re.sub(' +',' ', citation.replace("\n","").replace("\r","").strip())
+
+            if (i is not 1):
+                f.write(',\n')
+            f.write('{\n'
+                    '   "id":"'+ id +'",\n'
+                    '   "name":"'+ name +'",\n'
+                    '   "year":'+ year + ",\n"
+                    '   "citation":"'+ citation + '"\n'
+                    '}')
+            i= i+1
+
+        except:
+            e.write(str(item))
+            pass
+
+    f.write("]")
+    print(i-1)
--- python/getItems.py
+++ python/getItems.py
@@ -8,15 +8,21 @@ table = tree.xpath('//table[@class="browsing"]')[0]
 rows = table.xpath('//tr')
 
 with open('items.json', 'w') as f:
+    f.write("[")
     for i in range(1,len(rows)):
         row = rows[i]
         cam = row[0].text_content().encode('utf-8').strip()
-        title = row[1].text_content().encode('utf-8').strip()
+        title = row[1].text_content().encode('utf-8').strip().replace("\n","")
+        if (cam[-2:] == "EM"):
+            supplied = "true"
+        else: supplied = "false"
         #print(cam, title)
         if (i is not 1):
             f.write(',\n')
         f.write('{\n'
                 '   "id":"_id",\n'
                 '   "cameron":"'+ cam +'",\n'
-                '   "title":"'+ title + '"\n'
+                '   "title":"'+ title + '",\n'
+                '   "supplied":' + supplied + "\n"
                 '}')
+    f.write("]")
